574750902d8e7a801bd34a3535777284df0aa6bb
Israel Shedrack <israelshedrack913@gmail.com>
2024-07-06 14:24:27 +0100

    Finished models, established relationship between models: creating routes testing and register

--- models.py
+++ models.py
@@ -11,8 +11,19 @@ class Users(db.Model):
     email = db.Column(db.String(255), nullable = False, unique = True)
     password = db.Column(db.String(225), nullable = False)
     phone = db.Column(db.String(50))
+    organisation = db.relationship('Organisation', backref='users')
 
     def __init__(self, firstname, lastname, email):
         self.firstName = firstname
         self.lastName = lastname
         self.email = email
+
+class Organisation(db.Model):
+    orgId = db.Column(db.String(255), unique = True, default = create_id)
+    name = db.Column(db.String(255), nullable = False, required = True)
+    description = db.Column(db.String(255))
+    userId = db.Column(db.String(255), db.ForeignKey('users.id'))
+
+    def __init__(self, name, descp):
+        self.name = name
+        self.description = descp
