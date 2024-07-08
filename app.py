0631139af0da2bcec9966872b2c9758a62dc044f
Israel Shedrack <israelshedrack913@gmail.com>
2024-07-08 01:34:18 +0100

    new changes to app.py, modified code structure

--- app.py
+++ app.py
@@ -3,15 +3,25 @@ from extensions import jwt, db
 from config import AppConfig
 from route import root_route
 
-app = Flask(__name__)
-app.config.from_object(AppConfig)
+def create_app():
+
+    app = Flask(__name__)
+    with app.app_context():
+        app.config.from_object(AppConfig)
+        db.init_app(app)
+        from models import Users, Organisation
+        db.create_all()
+        jwt.init_app(app)
+    
+    return app
 
 #routes init
+app = create_app()
 root_route(app)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.init_app(app)
-        db.create_all()
-        jwt.init_app(app)
+    # with app.app_context():
+    #     db.init_app(app)
+    #     db.create_all()
+    #     jwt.init_app(app)
     app.run(debug=True)
