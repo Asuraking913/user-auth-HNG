cc165b9012c2895e37263195fc105d32854a573c
Israel Shedrack <israelshedrack913@gmail.com>
2024-07-09 11:36:13 +0100

    new changes

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 load_dotenv()
 
 class AppConfig:
-    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres.qfkcizyvmvolluzkqxqf:CdVAy16F9ej0lZS9@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.qfkcizyvmvolluzkqxqf:CdVAy16F9ej0lZS9@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     JWT_SECRET_KEY = "#@$@#$@#$@#$@#$@$#@#$@#$@#$@$##@$@#$%##^$^FDHFH#$^%$#W#"
